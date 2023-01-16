24ac276dee0aa2c01f6fa13efa3f8606e37e19ba
王彦为 <wyw2012@126.com>
2023-01-16 15:46:40 +0800

    Update test code
    1. Add window demo
    2. Update main picture

---
+++ test/ViewModels/WindowViewModel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WYW.UI.Test.Views;
+using WYW.UI.Test.Views.Windows;
+
+namespace WYW.UI.Test.ViewModels
+{
+    class WindowViewModel:ObservableObject
+    {
+        public RelayCommand NewNormalWindowCommand { get; } = new RelayCommand(() =>
+        {
+             new NormalWindow().Show();
+        });
+        public RelayCommand NewDefaultWindowCommand { get; } = new RelayCommand(() =>
+        {
+            new DefaultWindow().Show();
+        });
+        public RelayCommand NewNoTitleBarWindowCommand { get; } = new RelayCommand(() =>
+        {
+            new NoTitleBarWindow().Show();
+        });
+        public RelayCommand NewWithMenuWindowCommand { get; } = new RelayCommand(() =>
+        {
+            new WithMenuWindow().Show();
+        });
+        public RelayCommand NewCornerRadiusWindowCommand { get; } = new RelayCommand(() =>
+        {
+            new CornerRadiusWindow().Show();
+        });
+    }
+}
---
+++ test/Views/Windows/NormalWindow.xaml.cs
@@ -0,0 +1,13 @@
+﻿namespace WYW.UI.Test.Views.Windows
+{
+    /// <summary>
+    /// NormalWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class NormalWindow : Window
+    {
+        public NormalWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
